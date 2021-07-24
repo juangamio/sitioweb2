0f35eb03154ff18ab679e38f29243f433309ca3c
juangamio <63886165+juangamio@users.noreply.github.com>
2021-07-24 11:28:25 -0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -6,8 +6,8 @@ var prevScrollpos = window.pageYOffset;
             document.getElementById("navbar").style.top = "0";
 
       } else {
-            document.getElementById("navbar").style.top = "-100px";
-            document.getElementById("navbar").style.borderBottom = " 1px solid #0d60aba1";
+            //document.getElementById("navbar").style.top = "-100px";
+            document.getElementById("navbar").style.borderBottom = " 1px dotted #0d60aba1";
             //document.getElementById("navbar").style.background = "#ff5722";
        }
       prevScrollpos = currentScrollpos;
